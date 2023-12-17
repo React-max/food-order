60905cb93fd3023c5c910ed805b655a6dff4187d
Md. Abid Raihan <abidcsedu@gmail.com>
2023-12-17 18:27:38 +0600

    outputing cart items (halfway)

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,10 +1,16 @@
+import { useContext } from "react";
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 function Cart(props) {
+	const cartCtx = useContext(CartContext);
+
+	const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+
 	const cartItems = (
 		<ul className={classes["cart-items"]}>
-			{[{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }].map((item) => (
+			{cartCtx.items.map((item) => (
 				<li key={item.id}>{item.name}</li>
 			))}
 		</ul>
@@ -15,7 +21,7 @@ function Cart(props) {
 			{cartItems}
 			<div className={classes.total}>
 				<span>Total Amount</span>
-				<span>35.62</span>
+				<span>{totalAmount}</span>
 			</div>
 			<div className={classes.actions}>
 				<button className={classes["button--alt"]} onClick={props.onClose}>
